1ac3da0bc6e333510bb9c4eadc1ec9c99965e1e4
Afrakumah <opokua715@gmail.com>
2024-02-10 18:42:18 +0000

    create db and usermodel

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config();
+import connectDb from './database.js';
 
 
 const app = express()
+const PORT = process.env.PORT || 1000;
 
-app.listen(1000, () => {
-    console.log('server is running on port 1000')
+app.listen(PORT, () => {
+    connectDb()
+    console.log(`server is running on PORT ${PORT}`)
 });
